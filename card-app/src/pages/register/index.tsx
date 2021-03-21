64aa8681898ea0df8df342384a7cbe41432e7d47
NancyTang696 <nancy.tang2021@gmail.com>
2021-03-21 15:14:24 +1300

    add react hook form for credit card register

--- card-app/src/pages/register/index.tsx
+++ card-app/src/pages/register/index.tsx
@@ -3,6 +3,10 @@ import { MenuIconButton, Header } from '../../common/components/header'
 import { Controller, FormProvider, useForm } from "react-hook-form";
 import CreditCard from "../../common/types/credit-card";
 import { useAppDispatch, useAppSelector } from '../../store'
+import ReactDatePicker from "react-datepicker";
+import Button from '@material-ui/core/Button';
+import "react-datepicker/dist/react-datepicker.css";
+import styles from './styles.module.scss'
 
 type CardStringProperties = "creditCardNumber" | "CVC";
 type CardProperties = CardStringProperties | "expiryDate"
@@ -19,12 +23,92 @@ export const RegisterCreditCard = () => {
     const card = useAppSelector(state => state.card);
     const user = useAppSelector(state => state.user);
 
+    const getNumberField = (name: CardStringProperties, fieldDescription: string): JSX.Element => {
+        return (
+            <div>
+                <div>
+                    <label htmlFor={name}>{fieldDescription}</label>
+                </div>
+                <div>
+                    <input type="text" id={name} name={name} defaultValue={card.cardDetails[name]}
+                        ref={register({
+                            required: true,
+                            pattern: {
+                                value: /^\d{1,50}$/,
+                                message: "Entered value does not match format"
+                            }
+                        })} />
+                </div>
+                {errors[name] && errors[name]?.type === "required" && <div className={styles.error}>{fieldDescription} required</div>}
+                {errors[name] && errors[name]?.type === "pattern" && <div className={styles.error}>Entered value does not match {fieldDescription} format</div>}
+            </div>
+        );
+    };
+
+    const getDateInput = (name: CardProperties): JSX.Element => {
+        return (
+            <Controller
+                name={name}
+                control={control}
+                rules={{ required: true }}
+                defaultValue=""
+                error={errors[name]}
+                render={(props) => (
+                    <ReactDatePicker
+                        className="input"
+                        placeholderText="Select date"
+                        onChange={(e) => props.onChange(e)}
+                        selected={props.value}
+                    />
+                )}
+            />
+        );
+    };
+
+    const getFields = () => {
+        const creditCardNumber: CardStringProperties = "creditCardNumber";
+        const expiryDate: CardProperties = "expiryDate";
+        const CVC: CardStringProperties = "CVC";
+
+        return (
+            <div>
+                <div>{getNumberField(creditCardNumber, "Credit Card Number")}</div>
+                <div>{getNumberField(CVC, "CVC")}</div>
+                <div>{getDateInput(expiryDate)}</div>
+            </div>
+        )
+    }
+
+    const handleRegister = (values: CreditCard) => {
+        console.log(values);
+    };
+
+
+    const renderBody = () => {
+        return (
+            <div className={styles.container}>
+                <div className={styles.userBox}>
+                    Wellcome {user.userProfile?.firstName}
+                </div>
+                <div>
+                    <FormProvider {...formMethods}>
+                        <form onSubmit={handleSubmit( handleRegister)} noValidate={true}>
+                            <div> {getFields()}</div>
+                            <Button variant="contained" type="submit">Register</Button>
+                        </form>
+                    </FormProvider>
+                </div>
+            </div>
+        )
+    }
+
 
     return (
         <div>
             <div>
                 <Header title="Register Card Form" children={<MenuIconButton />} />
             </div>
+            {renderBody()}
         </div>
     )
 }
